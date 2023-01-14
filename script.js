5c9b4362543e57322e4c08f2df60a5aed5710d15
jonas Limah <jonaslimahpessoal@gmail.com>
2023-01-13 22:10:40 -0300

    today we added events at modalFace and cart

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let modalkey = 0
+let cart = []
 let modalQt = 1;
 
 const c = (e) => document.querySelector(e);
 const cs = (e) => document.querySelectorAll(e);
-
+// listagem das pizzas
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c(".models .pizza-item").cloneNode(true);
@@ -16,6 +18,7 @@ pizzaJson.map((item, index)=>{
         event.preventDefault();
         let key = event.target.closest(".pizza-item").getAttribute("data-key");
         modalQt = 1;
+        modalkey =key;
         c(".pizzaBig img").src = pizzaJson[key].img
         c(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
         c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
@@ -38,7 +41,50 @@ pizzaJson.map((item, index)=>{
     })
 
     c('.pizza-area').append(pizzaItem)
+    
 });
+// eventos do modal
+
+function closeModal(){
+    c(".pizzaWindowArea").style.opacity = "0"
+    setTimeout(()=>{
+        c(".pizzaWindowArea").style.display = "none"
+    }, 500)
+    
+}
+// tentei a propriedade forEach porem nao tava conseguindo fechar o modal  mobile
+c(".pizzaInfo--cancelButton").addEventListener("click", closeModal)
+c(".pizzaInfo--cancelMobileButton").addEventListener("click", closeModal)
 
 
+c(".pizzaInfo--qtmais").addEventListener("click",()=>{
+    modalQt++;
+    c(".pizzaInfo--qt").innerHTML = modalQt
+    
 
+})
+c(".pizzaInfo--qtmenos").addEventListener("click",()=>{
+    if (modalQt > 1){
+        modalQt--;
+        c(".pizzaInfo--qt").innerHTML = modalQt
+    }
+    
+})
+
+cs(".pizzaInfo--size").forEach((size, sizeIndex)=>{
+   size.addEventListener("click", (e)=>{
+    c(".pizzaInfo--size.selected").classList.remove("selected");
+    size.classList.add("selected");
+   })
+})
+// adicionando carrinho
+c(".pizzaInfo--addButton").addEventListener("click",()=>{
+
+    let size = parseInt(c(".pizzaInfo--size.selected").getAttribute("data-key"))
+    cart.push({
+        id:pizzaJson[modalkey].id,
+        size,
+        qt:modalQt
+    });
+    closeModal();
+});
